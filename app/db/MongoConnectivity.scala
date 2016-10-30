31eb1d19e2e1e1a229bf61cbb4b8dedea75ebd68
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-10-30 14:03:38 +0000

    Simplify mongo connectivity.

--- app/db/MongoConnectivity.scala
+++ app/db/MongoConnectivity.scala
@@ -2,26 +2,12 @@ package db
 
 import javax.inject.Singleton
 
-import com.mongodb.ConnectionString
-import org.mongodb.scala.{MongoClient, MongoClientSettings}
-import org.mongodb.scala.connection.ClusterSettings
+import org.mongodb.scala.MongoClient
 
 @Singleton
 class MongoConnectivity {
 
-  val mongoSettings = MongoClientSettings.builder().build()
-
-  val connectionString = new ConnectionString("mongodb://localhost/?connectTimeoutMS=5000&waitQueueTimeoutMS=500")
-
-  val clusterSettings = ClusterSettings.builder()
-    .applyConnectionString(connectionString)
-    .build()
-
-  val mongoClientSettings = MongoClientSettings.builder()
-    .clusterSettings(clusterSettings)
-    .build()
-
-  lazy val mongoClient = MongoClient(mongoClientSettings)
+  lazy val mongoClient = MongoClient("mongodb://localhost/")
 
   def db = mongoClient.getDatabase("sdkman")
 
