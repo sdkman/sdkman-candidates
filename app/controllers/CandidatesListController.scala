150ddcd94ae013f1a3339e8bab4a2dbdcf2e880a
Marco Vermeulen <vermeulen.mp@gmail.com>
2017-03-25 15:40:58 +0000

    Cache candidate list responses for 6 hours.

--- app/controllers/CandidatesListController.scala
+++ app/controllers/CandidatesListController.scala
@@ -1,20 +1,25 @@
 package controllers
 
 import com.google.inject.Inject
+import play.api.cache.Cached
 import play.api.mvc.{Controller, _}
 import rendering.{CandidateListSection, PlainTextRendering}
 import repos.CandidatesRepo
 
+import scala.concurrent.duration._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class CandidatesListController @Inject()(candidatesRepo: CandidatesRepo) extends Controller {
+class CandidatesListController @Inject()(candidatesRepo: CandidatesRepo, cached: Cached) extends Controller {
 
-  def list = Action.async { request =>
-    candidatesRepo.findAllCandidates().map { candidates =>
-      val sections = candidates.map { candidate =>
-        new CandidateListSection(candidate) with PlainTextRendering
+  def list = cached("candidate-list").default(6.hours) {
+    Action.async { request =>
+      candidatesRepo.findAllCandidates().map { candidates =>
+        val sections = candidates.map { candidate =>
+          new CandidateListSection(candidate) with PlainTextRendering
+        }
+        Ok(views.txt.candidate_list(sections))
       }
-      Ok(views.txt.candidate_list(sections))
     }
   }
 }
