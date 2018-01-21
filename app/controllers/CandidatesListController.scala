32e545199d7ef13ff3a173e3d1c5b4fb87965392
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-01-21 09:41:03 +0000

    Remove candidate list cache.

--- app/controllers/CandidatesListController.scala
+++ app/controllers/CandidatesListController.scala
@@ -1,24 +1,22 @@
 package controllers
 
 import com.google.inject.Inject
-import play.api.cache.Cached
 import play.api.mvc.{Controller, _}
 import rendering.{CandidateListSection, PlainTextRendering}
 import repos.CandidatesRepo
 
-import scala.concurrent.duration._
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class CandidatesListController @Inject()(candidatesRepo: CandidatesRepo, cached: Cached) extends Controller {
+class CandidatesListController @Inject()(candidatesRepo: CandidatesRepo) extends Controller {
 
-  def list() = cached("candidate-list").default(5.minutes) {
-    Action.async { request =>
-      candidatesRepo.findAllCandidates().map { candidates =>
-        val sections = candidates.map { candidate =>
-          new CandidateListSection(candidate) with PlainTextRendering
+  def list() = Action.async { _ =>
+    candidatesRepo.findAllCandidates().map { candidates =>
+      Ok {
+        views.txt.candidate_list {
+          candidates.map { candidate =>
+            new CandidateListSection(candidate) with PlainTextRendering
+          }
         }
-        Ok(views.txt.candidate_list(sections))
       }
     }
   }
