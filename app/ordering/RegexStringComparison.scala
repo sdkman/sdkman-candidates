1e3c29ed3ec5f65a4c4d330e9ed5d3ecb4b92892
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-06-19 21:02:23 +0100

    Refine regex group extraction comparison.

--- app/ordering/RegexStringComparison.scala
+++ app/ordering/RegexStringComparison.scala
@@ -5,11 +5,15 @@ import scala.util.matching.Regex
 
 trait RegexStringComparison {
 
-  def compareRegexGroups(s1: String, s2: String)(implicit regex: Regex): Int = {
-    val regex(x1, x2, x3) = s1
-    val regex(y1, y2, y3) = s2
+  private val WeightedPlaceholder = "Z"
 
-    val zipped = List(x1, x2, x3) zip List(y1, y2, y3)
+  def compareRegexGroups(s1: String, s2: String)(implicit regex: Regex): Int = {
+    val l1 = regex.findAllMatchIn(s1).toList.map(_.toString)
+    val l2 = regex.findAllMatchIn(s2).toList.map(_.toString)
+
+    val max = Math.max(l1.length, l2.length)
+
+    val zipped = l1.padTo(max, WeightedPlaceholder) zip l2.padTo(max, WeightedPlaceholder)
 
     @tailrec
     def compare(segments: Seq[(String, String)]): Int = segments match {
--- test/ordering/RegexStringComparisonSpec.scala
+++ test/ordering/RegexStringComparisonSpec.scala
@@ -5,36 +5,48 @@ import org.scalatest.{Matchers, WordSpec}
 import scala.util.matching.Regex
 
 class RegexStringComparisonSpec extends WordSpec with Matchers {
-  "compare regex groups" should {
+  "comparison of extracted regex groups" when {
 
-    implicit val regex: Regex = """^(\d*)\.(\d*)\.(\d*)$""".r
+    implicit val regex: Regex = """(\d+|\w+)""".r
 
-    "determine equality by regex" in new RegexStringComparison {
-      compareRegexGroups("1.2.3", "1.2.3") shouldBe 0
+    "groups are numeric" should {
+
+      "determine equality by regex" in new RegexStringComparison {
+        compareRegexGroups("1.2.3", "1.2.3") shouldBe 0
+      }
+
+      "determine lexicographic inequality by regex in first segment" in new RegexStringComparison {
+        compareRegexGroups("1.0.0", "0.0.1") shouldBe 1
+        compareRegexGroups("0.0.1", "1.0.0") shouldBe -1
+      }
+
+      "determine lexicographic inequality by regex in second segment" in new RegexStringComparison {
+        compareRegexGroups("0.1.0", "0.0.1") shouldBe 1
+        compareRegexGroups("0.0.1", "0.1.0") shouldBe -1
+      }
+
+      "determine lexicographic inequality by regex in third segment" in new RegexStringComparison {
+        compareRegexGroups("0.0.2", "0.0.1") shouldBe 1
+        compareRegexGroups("0.0.1", "0.0.2") shouldBe -1
+      }
+
+      "determine lexicographic inequality by regex in unequal length groupings" in new RegexStringComparison {
+        compareRegexGroups("0.1", "0.0.1") shouldBe 1
+        compareRegexGroups("0.0.1", "0.1") shouldBe -1
+      }
     }
 
-    "determine right weighted lexographic inequality by regex in first segment" in new RegexStringComparison {
-      compareRegexGroups("0.0.1", "1.0.0") shouldBe -1
+    "groups are alpha or numeric" should {
+
+      "determine lexicographic inequality by regex with alpha segments" in new RegexStringComparison {
+        assert(compareRegexGroups("2.0.0.RC1", "2.0.0.M5") > 0)
+        assert(compareRegexGroups("2.0.0.M5", "2.0.0.M4") > 0)
+        assert(compareRegexGroups("2.0.0.M3", "2.0.0.M3") == 0)
+        assert(compareRegexGroups("2.0.0.M3", "2.0.0.RC1") < 0)
+        assert(compareRegexGroups("2.0.0.RELEASE", "2.0.0.RC1") > 0)
+        assert(compareRegexGroups("2.0.0", "2.0.0.RC1") > 0)
+      }
     }
 
-    "determine right weighted lexographic inequality by regex in second segment" in new RegexStringComparison {
-      compareRegexGroups("0.0.1", "0.1.0") shouldBe -1
-    }
-
-    "determine right weighted lexographic inequality by regex in third segment" in new RegexStringComparison {
-      compareRegexGroups("0.0.1", "0.0.2") shouldBe -1
-    }
-
-    "determine left weighted lexographic inequality by regex in first segment" in new RegexStringComparison {
-      compareRegexGroups("1.0.0", "0.0.1") shouldBe 1
-    }
-
-    "determine left weighted lexographic inequality by regex in second segment" in new RegexStringComparison {
-      compareRegexGroups("0.1.0", "0.0.1") shouldBe 1
-    }
-
-    "determine left weighted lexographic inequality by regex in third segment" in new RegexStringComparison {
-      compareRegexGroups("0.0.2", "0.0.1") shouldBe 1
-    }
   }
 }
