78c8a2fc8cb4f58ad699d936c4090122747797d2
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-07-01 16:55:48 +0100

    Add TODO note for handling platform specific versions.

--- app/controllers/VersionsListController.scala
+++ app/controllers/VersionsListController.scala
@@ -4,13 +4,15 @@ import javax.inject.Inject
 import ordering.VersionOrdering
 import play.api.mvc._
 import repos.VersionsRepository
+import utils.Platform
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class VersionsListController @Inject()(versionsRepo: VersionsRepository) extends Controller with VersionOrdering {
   def list(candidate: String, platform: String, current: Option[String], installed: List[String]) =
     Action.async(parse.anyContent) { request =>
-      versionsRepo.findAllVersionsByCandidatePlatform(candidate, "UNIVERSAL").map { versions =>
+      //TODO handle platform specific candidates
+      versionsRepo.findAllVersionsByCandidatePlatform(candidate, Platform.Universal.identifier).map { versions =>
         Ok(views.txt.version_list(candidate.capitalize, reverseOrder(versions)))
       }
     }
