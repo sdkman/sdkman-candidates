45512c58e55f9590c46f0e1a9c7c58db092c56b7
Damiaan van der Kruk <damiaan.van.der.kruk@gmail.com>
2020-06-28 20:50:59 +1000

    Add Linux ARM 32 and 64 bit platforms

--- app/utils/Platform.scala
+++ app/utils/Platform.scala
@@ -8,6 +8,8 @@ object Platform {
     case "linux" => Some(Linux64)
     case "linux64" => Some(Linux64)
     case "linux32" => Some(Linux32)
+    case "linuxarm64" => Some(LinuxARM64)
+    case "linuxarm32" => Some(LinuxARM32)
     case "darwin" => Some(MacOSX)
     case "freebsd" => Some(FreeBSD)
     case "sunos" => Some(SunOS)
@@ -19,6 +21,8 @@ object Platform {
 
   val Linux32 = Platform("LINUX_32", "Linux 32bit")
   val Linux64 = Platform("LINUX_64", "Linux 64bit")
+  val LinuxARM32 = Platform("LINUX_ARM32", "Linux ARM 32bit")
+  val LinuxARM64 = Platform("LINUX_ARM64", "Linux ARM 64bit")
   val MacOSX = Platform("MAC_OSX", "Mac OSX")
   val Windows64 = Platform("WINDOWS_64", "Cygwin")
   val FreeBSD = Platform("FREE_BSD", "FreeBSD")
