0ca3e9f10a5f89606c19a1c93e7408d89372346a
Eddú Meléndez <eddu.melendez@gmail.com>
2020-12-23 11:20:05 -0600

    Add Eclipse Temurin
    See https://blog.adoptopenjdk.net/2020/12/eclipse-adoptium-projects-ready-for-community-review/

--- app/controllers/JavaListController.scala
+++ app/controllers/JavaListController.scala
@@ -77,6 +77,7 @@ class JavaListController @Inject() (versionRepo: VersionsRepository, cc: Control
     "mandrel" -> "Mandrel",
     "ms"      -> "Microsoft",
     "sapmchn" -> "SAP",
+    "tem"     -> "Temurin",
     "trava"   -> "TravaOpenJDK",
     "zulu"    -> "Azul Zulu",
     "zulufx"  -> "Azul ZuluFX"
