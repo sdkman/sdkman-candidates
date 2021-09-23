5647932c5b967ca222f0592ff3f30418ecc5ba43
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-09-23 18:11:06 +0100

    Code formatting.

--- test/controllers/LocalVersionFilteringSpec.scala
+++ test/controllers/LocalVersionFilteringSpec.scala
@@ -8,7 +8,12 @@ import play.api.Logging
 
 import scala.util.Random
 
-class LocalVersionFilteringSpec extends AnyWordSpec with Matchers with ScalaCheckDrivenPropertyChecks with Checkers with Logging {
+class LocalVersionFilteringSpec
+    extends AnyWordSpec
+    with Matchers
+    with ScalaCheckDrivenPropertyChecks
+    with Checkers
+    with Logging {
 
   "Local version filtering" should {
 
@@ -17,37 +22,39 @@ class LocalVersionFilteringSpec extends AnyWordSpec with Matchers with ScalaChec
     val unknown = Seq("local", "oracle", "myxyz")
 
     val versionGen: Gen[String] = for {
-      maj <- Gen.numChar
-      min <- Gen.numChar
+      maj   <- Gen.numChar
+      min   <- Gen.numChar
       patch <- Gen.numChar
     } yield s"$maj.$min.$patch"
 
-    def suffixes(xs: Seq[String]): Gen[String] = for {
-      suffixes <- Gen.oneOf(xs)
-    } yield suffixes
+    def suffixes(xs: Seq[String]): Gen[String] =
+      for {
+        suffixes <- Gen.oneOf(xs)
+      } yield suffixes
 
-    def identifiers(suffixGen: Gen[String]): Gen[String] = for {
-      version <- versionGen
-      suffix <- suffixGen
-    } yield s"$version-$suffix"
+    def identifiers(suffixGen: Gen[String]): Gen[String] =
+      for {
+        version <- versionGen
+        suffix  <- suffixGen
+      } yield s"$version-$suffix"
 
     val combinedVersions: Gen[(List[String], List[String])] = for {
-      knownVersions <- Gen.listOfN(5, identifiers(suffixes(known)))
+      knownVersions   <- Gen.listOfN(5, identifiers(suffixes(known)))
       unknownVersions <- Gen.listOfN(5, identifiers(suffixes(unknown)))
     } yield (knownVersions, unknownVersions)
 
-    "filter names not ending certain suffixes" in new JavaListController(null, null) {
+    "filter names not ending certain suffixes" in new JavaListController(null, null, null) {
 
       check {
-        Prop.forAll(combinedVersions) { case (kvs: Seq[String], ukvs: Seq[String]) =>
+        Prop.forAll(combinedVersions) {
+          case (kvs: Seq[String], ukvs: Seq[String]) =>
+            val allVersions = Random.shuffle(kvs ++ ukvs)
 
-          val allVersions = Random.shuffle(kvs ++ ukvs)
+            val names = findAllNotEndingWith(allVersions, known.toSet)
 
-          val names = findAllNotEndingWith(allVersions, known.toSet)
+            logger.info(allVersions + " -> " + names + " : ")
 
-          logger.info(allVersions + " -> " + names + " : ")
-
-          (names diff ukvs).isEmpty
+            (names diff ukvs).isEmpty
         }
       }
     }
