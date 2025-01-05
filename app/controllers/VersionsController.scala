e7b0c17e7971877eeeae2fcb4fb99011b01488d1
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-01-05 10:38:30 +0000

    Wire StateApi into VersionsController

--- app/controllers/VersionsController.scala
+++ app/controllers/VersionsController.scala
@@ -1,26 +1,19 @@
 package controllers
 
+import clients.StateApi
 import com.google.inject.Inject
 import play.api.mvc._
-import repos.{CandidatesRepository, VersionsRepository}
 import utils.Platform
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class VersionsController @Inject() (
-    versionsRepo: VersionsRepository,
-    candidatesRepo: CandidatesRepository,
-    cc: ControllerComponents
-) extends AbstractController(cc) {
+class VersionsController @Inject() (stateApi: StateApi, cc: ControllerComponents)
+    extends AbstractController(cc) {
 
-  def all(candidate: String, platformId: String) = Action.async(parse.anyContent) { request =>
-    candidatesRepo.findCandidate(candidate).flatMap { candidateO =>
-      val universal    = candidateO.map(_.distribution).contains("UNIVERSAL")
-      val distribution = if (universal) "UNIVERSAL" else Platform(platformId).distribution
-
-      versionsRepo.findAllVersionsByCandidatePlatform(candidate, distribution).map { versions =>
+  def all(candidate: String, platformId: String): Action[AnyContent] =
+    Action.async(parse.anyContent) { request =>
+      stateApi.findVersionsByCandidatePlatform(candidate, Platform(platformId)).map { versions =>
         Ok(versions.map(_.version).mkString(","))
       }
     }
-  }
 }
