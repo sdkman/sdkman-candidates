06216963ddc9e645c6c4df17a8d4ebfbc96b97ce
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-01-21 23:02:00 +0000

    First steps moving off Mongo Versions collection.

--- test/steps/Env.scala
+++ test/steps/Env.scala
@@ -1,17 +1,26 @@
 package steps
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
 import cucumber.api.scala.ScalaDsl
 import play.api.Logging
 import play.test.Helpers.testServer
+import play.test.TestServer
 import support.Mongo
 
 class Env extends ScalaDsl with Logging {
-  val app = testServer(9000)
+
+  val wireMockServer = new WireMockServer(options().port(8080))
+  wireMockServer.start()
+
+  val app: TestServer = testServer(9000)
   app.start()
 
   Before { _ =>
     Mongo.dropAllCollections()
     Mongo.insertAliveOk()
+    WireMock.reset()
     World.currentVersion = ""
     World.installedVersions = List.empty
   }
@@ -19,5 +28,6 @@ class Env extends ScalaDsl with Logging {
   sys.addShutdownHook {
     logger.info("Shutting down test server...")
     app.stop()
+    wireMockServer.stop()
   }
 }
