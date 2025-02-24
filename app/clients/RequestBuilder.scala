5ab2e53cd7f203fcef7eb9c15463a5b6a928d6c3
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-02-24 13:31:17 +0000

    Introduce hidden parameter to request builder method

--- app/clients/RequestBuilder.scala
+++ app/clients/RequestBuilder.scala
@@ -17,9 +17,13 @@ class RequestBuilder @Inject() (config: Configuration, ws: WSClient) {
   private lazy val stateApi =
     s"${stateApiConfig("protocol")}://${stateApiConfig("host")}:${stateApiConfig("port")}"
 
-  def versionsByCandidatePlatformRequest(candidate: String, platform: String): WSRequest =
+  def versionsByCandidatePlatformRequest(
+      candidate: String,
+      platform: String,
+      hidden: Boolean = false
+  ): WSRequest =
     ws.url(s"$stateApi/versions/$candidate")
-      .withQueryStringParameters(("distribution", platform), ("hidden", "false"))
+      .withQueryStringParameters(("distribution", platform), ("hidden", hidden.toString))
       .addHttpHeaders("Accept" -> "application/json")
       .withRequestTimeout(1500.millis)
 }
