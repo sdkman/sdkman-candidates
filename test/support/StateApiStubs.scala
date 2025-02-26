d2f8f6ae906d07f15db1cd44e61a624c3f1f7c53
Marco Vermeulen <vermeulen.mp@gmail.com>
2025-02-26 18:01:36 +0000

    Cut version validation over to state API

--- test/support/StateApiStubs.scala
+++ test/support/StateApiStubs.scala
@@ -1,12 +1,16 @@
 package support
 
+import clients.StateApiVersion
 import com.github.tomakehurst.wiremock.client.WireMock._
-import io.sdkman.repos.Version
+import io.sdkman.repos.{Candidate, Version}
+
+import scala.collection.JavaConverters._
 
 object StateApiStubs {
 
   import play.api.libs.json._
-  implicit val versionWrites: Writes[Version] = Json.writes[Version]
+  implicit val versionWrites: Writes[Version]                 = Json.writes[Version]
+  implicit val stateApiVersionWrites: Writes[StateApiVersion] = Json.writes[StateApiVersion]
 
   def stubVersionsForCandidateAndDistribution(
       candidate: String,
@@ -22,4 +26,56 @@ object StateApiStubs {
             .withStatus(200)
         )
     )
+
+  def stubVersionForCandidateAndDistribution(
+      candidate: String,
+      version: String,
+      distribution: String,
+      url: String,
+      vendor: Option[String]
+  ): Unit = {
+    val queryParams = List(
+      Some("distribution" -> equalTo(distribution)),
+      vendor.map(v => "vendor" -> equalTo(v))
+    ).flatten
+    stubFor(
+      get(urlPathEqualTo(s"/versions/$candidate/$version"))
+        .withQueryParams(queryParams.toMap.asJava)
+        .willReturn(
+          aResponse()
+            .withBody(
+              Json
+                .toJson(
+                  StateApiVersion(
+                    candidate = candidate,
+                    version = version,
+                    distribution = distribution,
+                    url = url,
+                    vendor = vendor,
+                    visible = true
+                  )
+                )
+                .toString
+            )
+            .withStatus(200)
+        )
+    )
+  }
+
+  def stubNoVersionForCandidateAndDistribution(
+      candidate: String,
+      version: String,
+      distribution: String,
+      vendor: Option[String]
+  ): Unit = {
+    val queryParams = List(
+      Some("distribution" -> equalTo(distribution)),
+      vendor.map("vendor" -> equalTo(_))
+    ).flatten
+    stubFor(
+      get(urlPathEqualTo(s"/versions/$candidate/$version"))
+        .withQueryParams(queryParams.toMap.asJava)
+        .willReturn(aResponse().withStatus(404))
+    )
+  }
 }
